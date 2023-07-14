1d70b8346e40f81922c3c44875f25ddec7c433d4
benzoesan <117587746+benzoesan@users.noreply.github.com>
2023-07-14 12:33:15 +0200

    entities and repositories

---
+++ src/main/java/pl/coderslab/charity/entity/Donation.java
@@ -0,0 +1,30 @@
+package pl.coderslab.charity.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Donation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity;
+    @ManyToMany
+    private List<Category> categories;
+    @ManyToOne
+    private Institution institution;
+    private String street;
+    private String city;
+    private String zipCode;
+    private LocalDate pickUpDate;
+    private LocalTime pickUpTime;
+    private String pickUpComment;
+}
